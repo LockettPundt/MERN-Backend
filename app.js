5b6632fc3a12106596af888280b2344f8aa75f84
Lockett Pundt <kalifornium@hotmail.com>
2020-05-14 15:12:05 -0400

    added the new route controller for addjob. require dotenv for mongoose db info when i get it going. imported mongoose.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const addJob = require('./routes/addjob');
+
+require('dotenv').config();
 
 const app = express();
 
@@ -13,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/addjob', addJob);
 
 module.exports = app;
