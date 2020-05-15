f8316fb55670c8f6e048e8fc1b3265715462f123
Lockett Pundt <kalifornium@hotmail.com>
2020-05-15 10:46:32 -0400

    imported cors

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const jobsRouter = require('./routes/jobs');
+
 
 require('dotenv').config();
 
@@ -21,7 +23,7 @@ mongoose.connect(process.env.DB_URI, {
 
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
