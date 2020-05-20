c154cbd278195f6883f090fce6a76bd708324b0c
Lockett Pundt <kalifornium@hotmail.com>
2020-05-20 12:47:15 -0400

    deleted the commented out stuff. working on a function to better handle the user auth calls.

--- routes/userauth.js
+++ routes/userauth.js
@@ -5,27 +5,12 @@ require('dotenv').config();
 const jwtSecret = process.env.JWT_SECRET;
 const router = express.Router();
 
-// I'll refactor the middleware to work.
-
-// const checkToken = (req, res, next) => {
-//   const header = req.headers.authorization;
-//   console.log(req.headers.authorization);
-//   if (typeof header !== 'undefined') {
-//     const bearer = header.split(' ');
-//     const token = bearer[1];
-//     req.token = token;
-//     next();
-//   } else {
-//     res.sendStatus(403);
-//   }
-// };
 
 router.post('/', (req, res) => {
   console.log('this is the auth.', req.body.userToken);
   const { userToken } = req.body;
   jwt.verify(userToken, jwtSecret, (err, authorizedData) => {
     if (err) {
-      // If error send Forbidden (403)
       console.log('ERROR: Could not connect to the protected route');
       res.sendStatus(403);
     } else {
