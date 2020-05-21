4e736a01aa22bd9b4673694225fd5abebab11c38
Lockett Pundt <kalifornium@hotmail.com>
2020-05-21 16:23:56 -0400

    leaving console logs in user auth until deployment.

--- routes/userauth.js
+++ routes/userauth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -5,21 +6,21 @@ require('dotenv').config();
 const jwtSecret = process.env.JWT_SECRET;
 const router = express.Router();
 
-// toure to authenticate user jwt.
+// route to authenticate user jwt.
 
 router.post('/', (req, res) => {
-  console.log('this is the auth.', req.body.userToken);
+  // console.log('this is the auth request.', req.body.userToken);
   const { userToken } = req.body;
   jwt.verify(userToken, jwtSecret, (err, authorizedData) => {
     if (err) {
-      // console.log('ERROR: Could not connect to the protected route');
+      console.log('ERROR: Could not connect to the protected route');
       res.json({ error: 'Invalid Token.' });
     } else {
       res.json({
         message: 'Successful log in',
         authorizedData,
       });
-      // console.log('SUCCESS: Connected to protected route');
+      console.log('SUCCESS: Connected to protected route');
     }
   });
 });
